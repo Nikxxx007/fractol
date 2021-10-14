57360d88b2c4dafd206a1b31b33b627dc9f70f43
Nick Nefed <nicknefed@MacBook-Pro-Nick.local>
2021-10-14 23:59:16 +0300

    fract drawing func in progress

--- includes/fractol.h
+++ includes/fractol.h
@@ -6,10 +6,23 @@
 # include <stdlib.h>
 # include <math.h>
 # include "../libmlx/mlx.h"
+# include "libft.h"
 
 #include <OpenGL/gl.h>
 #include <OpenGl/glu.h>
 #include <GLUT/glut.h>
+
+typedef struct s_vars
+{
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+	char	*addr;
+	char 	name;
+	void	*mlx;
+	void	*win;
+	void	*img;
+}				t_vars;
 
 
 #endif //FRACTOL_FRACTOL_H

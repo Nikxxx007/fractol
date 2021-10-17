be87e84045858350db2b05c6547f04e6f3e49115
Nick <nikita.n007@mail.ru>
2021-10-17 16:03:11 +0300

    mandelbrot is ready

--- includes/fractol.h
+++ includes/fractol.h
@@ -8,12 +8,32 @@
 # include "../libmlx/mlx.h"
 # include "libft.h"
 
-#include <OpenGL/gl.h>
-#include <OpenGl/glu.h>
-#include <GLUT/glut.h>
+# include <OpenGL/gl.h>
+# include <OpenGl/glu.h>
+# include <GLUT/glut.h>
+
+# define WIN_H 600
+# define WIN_W 800
 
 typedef struct s_vars
 {
+//	float				infinity;
+//	int					iteration;
+//	long double			min_x;
+	float			pos_x;
+	float 			pos_y;
+
+	int 		select[3];
+	uint8_t 	color[9][3];
+//	long double			max_y;
+//
+//	int		r;
+//	int 	g;
+//	int		b;
+
+	int		iter;
+	float 	zoom;
+
 	int		bits_per_pixel;
 	int		line_length;
 	int		endian;
@@ -21,8 +41,11 @@ typedef struct s_vars
 	char 	name;
 	void	*mlx;
 	void	*win;
+
 	void	*img;
 }				t_vars;
+
+int expose_hook(t_vars *vars);
 
 
 #endif //FRACTOL_FRACTOL_H
--- src/main.c
+++ src/main.c
@@ -5,20 +5,119 @@
 //	return(0);
 //}
 
-int		mandelbrot(t_vars *vars)
+uint32_t		fractol_degr(t_vars *vars, int i, unsigned int select)
+{
+	uint8_t		rgb[3];
+
+	rgb[0] = vars->color[vars->select[select]][0];
+	rgb[1] = vars->color[vars->select[select]][1];
+	rgb[2] = vars->color[vars->select[select]][2];
+	return ((rgb[0] * i / vars->iter) * 256 * 256 +
+			(rgb[1] * i / vars->iter) * 256 +
+			(rgb[2] * i / vars->iter));
+}
+
+void	my_mlx_pixel_put(t_vars *vars, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = vars->addr + (y * vars->line_length + x * (vars->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+} //my_mlx_pixel_put(&vars, 5, 5, 0x00FF0000);
+
+uint32_t fract_calculation(t_vars *vars, int x, int y)
+{
+	long double mx;
+	long double my;
+	long double c_x;
+	long double c_y;
+	long double x_temp;
+	int 	i;
+
+	i = 0;
+
+	mx = 0.0;
+	my = 0.0;
+//	c_re = ft_map(x, add_params(0, WIDTH, e->min_x, e->max_x));
+//	c_x = vars->min_x + (vars->max_x - vars->min_x) * (x / WIN_W);
+//	c_im = ft_map(y, add_params(0, HEIGHT, e->min_y, e->max_y));
+//	c_y = vars->min_y + (vars->max_y - vars->min_y) * (y / WIN_H);
+
+	c_x = ((x * 3.5 / 800.0) - 1.75) / vars->zoom + vars->pos_x - 0.75;
+	c_y = ((y * 2.0 / 600.0) - 1) / vars->zoom + vars->pos_y;
+	while (mx * mx + my * my <= 4 && i < vars->iter)
+	{
+		x_temp = mx * mx - my * my;
+		my = 2 * mx * my + c_y;
+		mx = x_temp + c_x;
+		i++;
+	}
+//	if (i == vars->iter)
+//		return (fractol_degr(vars, i, 0));
+//	else if (i > vars->iter / 4)
+//		return (fractol_degr(vars, i, 1));
+//	else
+//		return (fractol_degr(vars, i, 2));
+	if (i % 4 == 0)
+		return (0x00000000);
+	else if (i % 4 == 1)
+		return (0x00FF0000);
+	else if (i % 4 == 2)
+		return (0x0000FF00);
+	else
+		return (0x000000FF);
+
+}
+
+int		mouse_hook(int keycode, int x, int y, t_vars *vars)
+{
+	double wheel;
+
+	wheel = 0;
+//	printf("   %d %d   ", x, y);
+	if (keycode == 5)
+	{
+		wheel = 1.0;
+		vars->zoom *= 2.0 * wheel;
+	}
+	if (keycode == 4)
+	{
+		wheel = -1;
+		vars->zoom /= 2.0 * fabs(wheel);
+	}
+	if (wheel != 0)
+	{
+		vars->pos_x = ((x * 3.5 / 800.0) - 1.75) / vars->zoom + vars->pos_x;
+		vars->pos_y = ((y * 2.0 / 600.0) - 1) / vars->zoom + vars->pos_y;
+//		vars->pos_x = (float)x;
+//		vars->pos_y = (float)y;
+		mlx_destroy_image(vars->mlx, vars->img);
+		mlx_clear_window(vars->mlx, vars->win);
+		expose_hook(vars);
+	}
+//	each_pixel(fractol, fractol->fractal[fractol->fnb]);
+	return (1);
+}
+
+int	mandelbrot(t_vars *vars)
 {
 	int	x;
 	int y;
-	int	color;
+	unsigned int	color;
 
 	y = 0;
-	while (y < 1200)
+	while (y < WIN_H)
 	{
 		x = 0;
-		while (x < 800)
+		while (x < WIN_W)
 		{
-			color =
+//			printf("   %d %d   ", x, y);
+
+			color = fract_calculation(vars, x, y);
+			my_mlx_pixel_put(vars, x, y, color);
+			x++;
 		}
+		y++;
 	}
 
 	return(0);
@@ -26,7 +125,7 @@ int		mandelbrot(t_vars *vars)
 
 int expose_hook(t_vars *vars)
 {
-	vars->img = mlx_new_image(vars->mlx, 1280, 800);
+	vars->img = mlx_new_image(vars->mlx, WIN_W, WIN_H);
 	vars->addr = mlx_get_data_addr(vars->img, &vars->bits_per_pixel, &vars->line_length,
 								 &vars->endian);
 
@@ -38,6 +137,7 @@ int expose_hook(t_vars *vars)
 //	if (vars->name == 'j')
 //		julia(vars);
 
+	mlx_do_sync(vars->win);
 	mlx_put_image_to_window(vars->mlx, vars->win, vars->img, 0, 0);
 
 //	text for user can be added here
@@ -66,14 +166,15 @@ int	exit_hook(int keycode, t_vars vars)
 void win_manager(t_vars *vars)
 {
 	vars->mlx = mlx_init();
-	vars->win = mlx_new_window(vars->mlx, 1200, 800, "fractol");
+	vars->win = mlx_new_window(vars->mlx, WIN_W, WIN_H, "fractol");
 
 	mlx_hook(vars->win, 12, 0, expose_hook, vars);
+//	expose_hook(vars);
 	mlx_hook(vars->win, 2, 0, key_hook, vars);
 	mlx_hook(vars->win, 17, 0, exit_hook,vars);
 
 
-//	mlx_mouse_hook(vars->win, func_for_mouse, vars); TODO add zoom in and out function
+	mlx_mouse_hook(vars->win, mouse_hook, vars); //TODO add zoom in and out function
 
 	mlx_loop(vars->mlx);
 }
@@ -81,6 +182,16 @@ void win_manager(t_vars *vars)
 void v_fill(t_vars *vars)
 {
 	vars->name = 'n';
+	vars->iter = 250;
+	vars->zoom = 0.5;
+
+	vars->pos_x = 0;
+	vars->pos_y = 0;
+//	vars->min_x = -1.5f;
+//	vars->max_x = 1.0f;
+//	vars->min_y = -1.0f;
+//	vars->max_y = 1.0f;
+//	vars->infinity = 10;
 }
 
 int	main(int argc, char **argv)
